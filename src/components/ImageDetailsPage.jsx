5d8e52e175b6dca598f980d2507979873cf264bc
Christina Woodhams <cwoodhams89@gmail.com>
2023-10-17 06:10:44 -0400

    add image to ImageDetailsPage

--- src/components/ImageDetailsPage.jsx
+++ src/components/ImageDetailsPage.jsx
@@ -3,11 +3,15 @@ export function ImageDetails({ art, onClose }) {
 		return null;
 	}
 
-	const { title, artist_title } = art;
+	const { title, artist_title, thumbnail, image_id } = art;
 
 	return (
 		<div>
 			<h1>{title}</h1>
+			<img
+				alt={thumbnail.alt_text}
+				src={`https://www.artic.edu/iiif/2/${image_id}/full/843,/0/default.jpg`}
+			/>
 			<p>Artist: {artist_title || 'Unknown Artist'}</p>
 			<button onClick={onClose}>Back</button>
 		</div>
